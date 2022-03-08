d2799114c5d33d1d2a249baeee3c7756268dbef7
andretgsilveira <andre_tgsilveira@hotmail.com>
2022-03-07 21:35:28 -0300

    Primeiro commit. Criado pacote para teste do desing patter Singleton

---
+++ src/one/dio/gof/singleton/SingletonLazyHolder.java
@@ -0,0 +1,17 @@
+package one.dio.gof.singleton;
+
+public class SingletonLazyHolder {
+
+    private static class InstanceHolder{
+        public static SingletonLazyHolder instance = new SingletonLazyHolder();
+    }
+
+
+    private SingletonLazyHolder(){
+        super();
+    }
+
+    public static SingletonLazyHolder getInstance(){
+        return InstanceHolder.instance;
+    }
+}
---
+++ src/one/dio/gof/singleton/TestSingleton.java
@@ -0,0 +1,24 @@
+package one.dio.gof.singleton;
+
+//  Teste Desing Patter Singleton
+
+public class TestSingleton {
+    public static void main(String[] args) {
+        SingletonLazy lazy = SingletonLazy.getInstance();
+        System.out.println(lazy);
+        lazy = SingletonLazy.getInstance();
+        System.out.println(lazy);
+
+        SingletonEager eager = SingletonEager.getInstance();
+        System.out.println(eager);
+        eager = SingletonEager.getInstance();
+        System.out.println(eager);
+
+        SingletonLazyHolder holder = SingletonLazyHolder.getInstance();
+        System.out.println(holder);
+        holder = SingletonLazyHolder.getInstance();
+        System.out.println(holder);
+    }
+
+
+}
